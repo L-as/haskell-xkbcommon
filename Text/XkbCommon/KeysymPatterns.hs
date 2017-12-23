06534b4f808446412f6c61e2e7b86747ed90e372
Markus Ongyerth <ongy@ongy.net>
2017-12-23 16:37:58 +0100

    Remove the KeysymPattern.hs TH file
    This reverts commit d5a460d10c9da8650ab4ced6b4a0735cf12ad741.

--- Text/XkbCommon/KeysymPatterns.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE TemplateHaskell, PatternSynonyms #-}
-module Text.XkbCommon.KeysymPatterns where
-
-import Text.XkbCommon.InternalTypes
-import Text.XkbCommon.ParseDefines
-
-$(getKeysymPats)
