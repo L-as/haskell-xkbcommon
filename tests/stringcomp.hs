c1475a6b6e344112e8e077e3f66baeed9d5c20b9
Las Safin <las@protonmail.ch>
2021-06-19 15:44:49 +0000

    Reformat tests/stringcomp.hs

--- tests/stringcomp.hs
+++ tests/stringcomp.hs
@@ -1,27 +1,25 @@
+import Common
 import Control.Monad
 import Data.Maybe
-
 import Text.XkbCommon
 
-import Common
-
 main = do
-   ctx <- liftM fromJust $ newContext defaultFlags
+  ctx <- liftM fromJust $ newContext defaultFlags
 
-   original <- readFile (datadir++"keymaps/stringcomp.data")
+  original <- readFile (datadir ++ "keymaps/stringcomp.data")
 
-   let keymap = newKeymapFromString ctx original
-   case keymap of
-		Nothing -> assert False "Could not read keymap string!"
-		Just _ -> assert True undefined
+  let keymap = newKeymapFromString ctx original
+  case keymap of
+    Nothing -> assert False "Could not read keymap string!"
+    Just _ -> assert True undefined
 
-   let dump = keymapAsString $ fromJust keymap
+  let dump = keymapAsString $ fromJust keymap
 
-   assert (dump == original) "original and dump not equal!"
+  assert (dump == original) "original and dump not equal!"
 
-   let keymap2 = newKeymapFromString ctx dump
-   let dump2 = keymapAsString $ fromJust keymap2
+  let keymap2 = newKeymapFromString ctx dump
+  let dump2 = keymapAsString $ fromJust keymap2
 
-   assert (dump2 == dump) "dump and dump2 not equal!"
+  assert (dump2 == dump) "dump and dump2 not equal!"
 
-   return ()
+  return ()
